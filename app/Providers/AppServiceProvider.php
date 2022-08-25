827d33381fea2ac719d1b066f74d54791a20d0dd
José Loarca <joseloarca97@icloud.com>
2022-08-24 22:51:12 -0600

    disable wrapping for apiresources

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Http\Resources\SettlementCollection;
+use App\Http\Resources\SettlementResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +28,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Remove 'data' wrapping
+        JsonResource::withoutWrapping();
+
+        // This is used to easily monitor the queries performed by zip-codes endpoint
+        if (config('app.debug')) {
+            DB::listen(function($query) {
+                Log::info($query->sql, [$query->bindings, $query->time]);
+            });
+        }
     }
 }
